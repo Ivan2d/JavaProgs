2672b029295e37789dd9a38e34d1fc97921cc88a
Ivan2d <ivan.romanovskiy.2002@mail.ru>
2021-11-15 20:05:17 +0600

    blin

---
+++ Payment.java
@@ -0,0 +1,133 @@
+package ru.imit.omsu;
+
+import java.util.Objects;
+
+public class Payment
+{
+private String fio;
+private int day;
+private int month;
+private int year;
+private double paysum;
+
+public Payment(String fio, int a, int b, int c, double sum)
+{
+    boolean fourthyear;
+    this.fio = fio;
+    if (((year % 4 == 0) && (year % 100) != 0) || (year % 400 == 0))
+    {
+        year = c;
+        fourthyear = false;
+    }
+    else
+    {
+        year = c;
+        fourthyear = true;
+    }
+    if (1 <= b && b <= 12) {
+        month = b;
+    } else {
+        throw new IllegalArgumentException("Некорректный месяц");
+    }
+    if (month == 2 && fourthyear == true) {
+        if (1 <= a && a <= 29) {
+            day = a;
+        } else {
+            throw new IllegalArgumentException("Некорректный день");
+        }
+    } else {
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+            if (1 <= a && a <= 31) {
+                day = a;
+            } else {
+                throw new IllegalArgumentException("Некорректный день");
+            }
+        } else {
+            if (month == 4 || month == 6 || month == 9 || month == 11) {
+                if (1 <= a && a <= 30) {
+                    day = a;
+                } else {
+                    throw new IllegalArgumentException("Некорректный день");
+                }
+            }
+        }
+    }
+    if (Double.compare(sum, 0) > 0 || Double.compare(sum, 0) == 0)
+    {
+        paysum = sum;
+    } else
+    {
+        throw new IllegalArgumentException("Некорректно введена сумма");
+    }
+}
+
+public Payment(final Payment payment)
+{
+    if (payment == null) {
+        throw new IllegalArgumentException("The payment is null");
+    }
+    this.fio = payment.fio;
+    this.day = payment.day;
+    this.month = payment.month;
+    this.year = payment.year;
+    this.paysum = payment.paysum;
+}
+
+public String getFio() {
+    return fio;
+}
+
+public int getDay() {
+    return day;
+}
+
+public int getMonth() {
+    return month;
+}
+
+public int getYear() {
+    return year;
+}
+
+public double getPaysum() {
+    return paysum;
+}
+
+public void setFio(String fio) {
+    this.fio = fio;
+}
+
+public void setDay(int day) {
+    this.day = day;
+}
+
+public void setMonth(int month) {
+    this.month = month;
+}
+
+public void setYear(int year) {
+    this.year = year;
+}
+
+public void setPaysum(double paysum) {
+    this.paysum = paysum;
+}
+
+
+@Override
+public boolean equals(Object o)
+{
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Payment payment = (Payment) o;
+    return day == payment.day && month == payment.month && year == payment.year && Double.compare(payment.paysum, paysum) == 0 && fio.equals(payment.fio);
+}
+
+@Override
+public int hashCode() {
+    return Objects.hash(fio, day, month, year, paysum);
+}
+}
+
+
+
---
+++ StringProcessor.java
@@ -0,0 +1,104 @@
+package ru.imit.omsu;
+
+import java.util.Stack;
+
+public class StringProcessor {
+public static String copystring(String s, int N) {
+    if (N < 0) throw new IllegalArgumentException("number of copies  can't be negative");
+
+    StringBuilder str = new StringBuilder("");
+
+    if (N == 0) return ("");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < s.length(); j++) {
+            str.append(s.charAt(j));
+        }
+    }
+    return new String(str);
+}
+
+public static int stringinsideanother(String n, String m) {
+    if (m.isEmpty()) throw new IllegalArgumentException("second string is empty");
+    if (m.length() == 0) throw new IllegalArgumentException("second string is null");
+    String p = n.replace(m, "");
+    int count = (n.length() - p.length()) / m.length();
+    return count;
+}
+
+public static String stringrecreatenumbers(String s) {
+    String a = s.replace("1", "один");
+    String b = a.replace("2", "два");
+    String c = b.replace("3", "три");
+
+    return new String(c);
+}
+
+public static String deletesymbol2(StringBuilder s) {
+    for (int i = 1; i <= s.length(); i += 1) {
+        s.deleteCharAt(i);
+    }
+    return s.toString();
+
+}
+
+public static String swapWordsstring(String s) {
+    if (s.isEmpty())
+        return s;
+    return swapWordsstring(s.substring(1)) + s.charAt(0);
+}
+
+public static long hexadecimalNumber(Stack<Character> number) {
+    if (number.isEmpty()) {
+        throw new IllegalArgumentException("Stack is empty");
+    }
+    long result = 0;
+    int rank = 0;
+    char symbol = 0;
+    while (!(number.isEmpty())) {
+        symbol = number.pop();
+        if (symbol >= 48 && symbol <= 57) {
+            result += (symbol - 48) * Math.pow(16, rank);
+        } else if (symbol >= 65 && symbol <= 70) {
+            result += (symbol - 55) * Math.pow(16, rank);
+        } else {
+            throw new IllegalArgumentException("This stack hasn't a hexadecimal number");
+        }
+        rank++;
+    }
+    return result;
+}
+
+public static String replaceHexadecimalNumbers(String str) {
+    if (str == null) {
+        throw new NullPointerException("The string is null");
+    }
+    Stack<Character> number = new Stack<>();
+    int i = 0;
+    int left = 0;
+    int right = 0;
+    long result = 0;
+    StringBuilder replacer = new StringBuilder(str);
+    while (i < replacer.length()) {
+        left = replacer.indexOf("0x");
+        if (left == -1) {
+            break;
+        } else {
+            left += 2;
+            right = left;
+            while (right != left + 8 && right != replacer.length()) {
+                number.push(replacer.charAt(right));
+                right++;
+            }
+            result = hexadecimalNumber(number);
+            left -= 2;
+            replacer.delete(left, right);
+            replacer.insert(left, result);
+            String letter = Long.toString(result);
+            i = left + letter.length() - 1;
+        }
+        i++;
+    }
+    return new String(replacer);
+}
+
+}
